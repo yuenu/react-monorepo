b90a69b04892586e6916ed8b19f1e4b26312cbf8
yuenu <miraiku82@gmail.com>
2023-02-14 12:32:52 +0800

    chore: setup test

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,9 +1,11 @@
-import { Button } from "ui";
+import { Calendar } from "calendar";
+import { Button } from 'button'
 
 export default function Web() {
   return (
     <div className="h-screen w-screen grid place-content-center">
       <Button />
+      <Calendar />
     </div>
   );
 }
---
+++ packages/calendar/Calendar.tsx
@@ -0,0 +1,4 @@
+import * as React from "react";
+export const Calendar = () => {
+  return <button>This is Calendar Component2</button>;
+};
