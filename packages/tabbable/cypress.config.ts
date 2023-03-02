a57feac296a732bb6e88dd24fb71f7f13de88ea9
yuenu <miraiku82@gmail.com>
2023-03-02 17:33:46 +0800

    feat: add tabbable

---
+++ packages/tabbable/cypress.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  viewportHeight: 600,
+  viewportWidth: 800,
+  video: false,
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "next",
+      bundler: "webpack",
+    },
+  },
+});
